4c3efe93deff85667e0ff5d087d7945d46f74476
uasmirnova <uasmirnova21@yandex.ru>
2022-09-18 21:12:15 +0300

    test added

---
+++ src/main/java/ru/netology/manager/TicketManager.java
@@ -0,0 +1,54 @@
+package ru.netology.manager;
+
+import ru.netology.repository.TicketRepository;
+import ru.netology.tickets.Ticket;
+
+import java.util.Arrays;
+
+public class TicketManager {
+
+    protected TicketRepository repo;
+
+    public TicketManager(TicketRepository repo) {
+        this.repo = repo;
+    }
+
+    public void add(Ticket ticket) {
+        repo.add(ticket);
+    }
+
+    public Ticket[] findAll() {
+        return repo.findAll();
+    }
+
+    public Ticket[] removeById(int id) {
+        repo.removeById(id);
+        return new Ticket[0];
+    }
+
+    public Ticket[] searchBy(String departureAirport, String arrivalAirport) {
+        Ticket[] result = new Ticket[0];
+        for (Ticket ticket : repo.findAll()) {
+            if (matches(ticket, departureAirport, arrivalAirport)) {
+                Ticket[] tmp = new Ticket[result.length + 1];
+                for (int i = 0; i < result.length; i++) {
+                    tmp[i] = result[i];
+                }
+                tmp[tmp.length - 1] = ticket;
+                result = tmp;
+            }
+        }
+        Arrays.sort(result);
+        return result;
+    }
+
+    public boolean matches(Ticket ticket, String departureAirport, String arrivalAirport) {
+        if (ticket.getDepartureAirport().equals(departureAirport)) {
+            if (ticket.getArrivalAirport().equals(arrivalAirport)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
---
+++ src/main/java/ru/netology/tickets/Ticket.java
@@ -0,0 +1,71 @@
+package ru.netology.tickets;
+
+public class Ticket implements Comparable<Ticket> {
+
+    protected int id;
+    protected int price;
+    protected String departureAirport;
+    protected String arrivalAirport;
+    protected int flightTime;
+
+    public Ticket(int id, int price, String departureAirport, String arrivalAirport, int flightTime) {
+        this.id = id;
+        this.price = price;
+        this.departureAirport = departureAirport;
+        this.arrivalAirport = arrivalAirport;
+        this.flightTime = flightTime;
+    }
+
+    @Override
+    public int compareTo(Ticket o) {
+       if (price < o.getPrice()) {
+           return -1;
+       } else {
+           if (price > o.getPrice()) {
+               return 1;
+           } else {
+               return 0;
+           }
+       }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public void setDepartureAirport(String departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public String getArrivalAirport() {
+        return arrivalAirport;
+    }
+
+    public void setArrivalAirport(String arrivalAirport) {
+        this.arrivalAirport = arrivalAirport;
+    }
+
+    public int getFlightTime() {
+        return flightTime;
+    }
+
+    public void setFlightTime(int flightTime) {
+        this.flightTime = flightTime;
+    }
+}
